8af6b592667bdab74959f3159b02c0304e6e2aad
Igor Korotkevitch <i.korotkevitch@gmail.com>
2022-11-19 07:36:40 +0300

    Added admin for user

--- user/models.py
+++ user/models.py
@@ -24,8 +24,8 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(max_length=255, unique=True)
-    name = models.CharField(max_length=255)
+    email = models.EmailField("Email", max_length=255, unique=True)
+    name = models.CharField("Имя", max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
